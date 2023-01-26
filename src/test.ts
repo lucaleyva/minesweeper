bc4f5c922efb9fc373c3e94ba802db627bade7fe
Luca Leyva <imlucaleyva@gmail.com>
2023-01-26 10:32:52 -0800

    parametric types and generics

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,40 @@
 let a: number;
 let b: boolean;
 let c: string;
+let d: Array<string>;
 
-type User = {
+interface BasicUser<A = boolean> {
   name: string;
   surname: string;
   age: number;
-  isAdmin: boolean;
-};
+  isAdmin: A;
+  permissions?: string[];
+}
 
-const user: User = {
+const user: BasicUser = {
   name: "Luca",
   surname: "Leyva",
   age: 37,
   isAdmin: true,
 };
 
-const multiplication = (a: number, b: number): number => a * b;
-const addition = (a: number, b: number): number => a + b;
+const user2: BasicUser = {
+  name: "Patrick",
+  surname: "Manelski",
+  age: 37,
+  isAdmin: true,
+  permissions: ["Manager", "Developer"],
+};
+
+let usersArray: BasicUser[] = [user, user, user];
+
+function getFirst<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirst<BasicUser>(usersArray);
+
+type MathFunc = (a: number, b: number) => number;
+
+const multiplication: MathFunc = (a, b) => a * b;
+const addition: MathFunc = (a, b) => a + b;
